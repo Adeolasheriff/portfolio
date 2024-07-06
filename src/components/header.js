80d9c16f4164c45e0f03cc41ec73de2ed9aee63b
Adeolasheriff <adeolasheriff3@gmail.com>
2024-07-06 11:44:38 +0100

    comm

--- src/components/header.js
+++ src/components/header.js
@@ -14,7 +14,7 @@ function Header() {
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
                     <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-                    {/* <Nav.Link href="/contact">Contact</Nav.Link> */}
+                    <Nav.Link href="/contact">Contact</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
